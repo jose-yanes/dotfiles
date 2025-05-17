a71ddeb54b6b743e384f24578547cb9ecc9183b6
Jose Yanes <113186145+jose-yanes@users.noreply.github.com>
2025-05-17 10:37:00 -0300

    Adding Mako

---
+++ nvim/.config/nvim/lua/plugins/extend-conform.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["typescript"] = { "biome" },
+        ["html"] = { "prettierd" },
+        ["css"] = { "prettierd" },
+      },
+    },
+  },
+}
