a3968454a3877523c8a1976971fabe6373486b9a
Jose Yanes <113186145+jose-yanes@users.noreply.github.com>
2025-05-20 10:34:35 -0300

    Commit

--- nvim/.config/nvim/lua/plugins/extend-conform.lua
+++ nvim/.config/nvim/lua/plugins/extend-conform.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         ["typescript"] = { "biome" },
+        ["javascript"] = { "biome" },
         ["html"] = { "prettierd" },
         ["css"] = { "prettierd" },
       },
